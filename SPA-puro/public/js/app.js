9034a0531bf07c3d7bf190268c5c557f1ae324b8
IsmaelR <ismael.dai@mail.com>
2015-12-08 21:55:13 +0100

    desarrollo ejemplo SPA puro

--- SPA-puro/public/js/app.js
+++ SPA-puro/public/js/app.js
@@ -1,24 +1,48 @@
-'use strict';
-
 angular.module('myApp', ['ui.router'])
   .config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/indice");
+    'use strict';
+
+    $urlRouterProvider.otherwise('/indice');
 
     $stateProvider
       .state('login', {
-        url        : "/login",
-        templateUrl: "views/login.html"
+        url        : '/login',
+        templateUrl: 'views/login.html'
       })
       .state('principal', {
-        url        : "/principal",
-        templateUrl: "views/principal.html"
+        url        : '/principal',
+        templateUrl: 'views/principal.html'
       })
       .state('admin', {
-        url        : "/admin",
-        templateUrl: "views/admin.html"
+        url        : '/admin',
+        templateUrl: 'views/admin.html'
       })
       .state('indice', {
-        url        : "/indice",
-        templateUrl: "views/indice.html"
+        url        : '/indice',
+        templateUrl: 'views/indice.html'
       });
-  });
+  })
+  .controller('FormController', ['$http', function ($http) {
+    'use strict';
+
+    var formCtrl = this;
+
+    formCtrl.formulario = {};
+
+    formCtrl.submit = function () {
+      $http.post('/login', {
+        // passport espera que los datos sean "username" y "password", si se llaman
+        // de otra forma, debemos indicarlo en la funcion de autenticacion, mirar
+        // la documentacion de passport
+        username: formCtrl.formulario.nombre,
+        password: formCtrl.formulario.password
+      }).then(function (success) {
+        1 + 1;
+      }, function (err) {
+        1 + 1;
+      });
+    };
+
+
+  }])
+;
--- SPA-puro/server.js
+++ SPA-puro/server.js
@@ -1,6 +1,13 @@
-var MongoClient = require('mongodb').MongoClient,
-    express     = require('express'),
-    path        = require('path');
+'use strict';
+
+var MongoClient   = require('mongodb').MongoClient,
+    express       = require('express'),
+    path          = require('path'),
+    passport      = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    bodyParser    = require('body-parser'),
+    cookieParser  = require('cookie-parser'),
+    session       = require('express-session');
 
 var url = 'mongodb://localhost:27017/spaAuthenticationExample';
 
@@ -29,16 +36,97 @@ MongoClient.connect(url, function (err, db) {
 
         console.log('Usuarios insertados correctamente');
 
+
+        passport.use(new LocalStrategy(
+          function (username, password, done) {
+            // a esta función se le llamará cuando haya que autenticar un usuario
+
+            coleccion.findOne({user: username}, function (err, user) {
+              console.log(user);
+
+              if (err) {
+                return done(err);
+              }
+
+              if (!user) {
+                return done(null, false, {message: 'Usuario incorrecto.'});
+              }
+
+              if (user.pass !== password) {
+                return done(null, false, {message: 'Password incorrecta.'});
+              }
+
+              return done(null, user);
+            });
+          }
+        ));
+
+        passport.serializeUser(function (user, done) {
+          console.log('serializeUser');
+          console.log(user);
+          done(null, user.id);
+        });
+
+        passport.deserializeUser(function (user, done) {
+          console.log('deserializeUser');
+          console.log(user);
+          coleccion.find(user.id, function (err, user) {
+            done(err, user);
+          });
+        });
+
+
         var app = express();
+
+        app.use(express.static(__dirname + '/public'));
+        app.use(express.static(__dirname + '/node_modules'));
+
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(bodyParser.json());
+
+        app.use(cookieParser());
+
+        app.use(session({secret: '7 caballo tiene bonanza'}));
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+
         app.set('view engine', 'html');
 
-        app.use('/', function (req, res) {
+
+        /******************************** ROUTING *****************************/
+        app.get('/', function (req, res) {
           res.type('text/html');
           res.sendFile(path.join(__dirname, '/public/views/' + 'index.html'));
         });
 
-        app.use(express.static(__dirname + '/public'));
-        app.use(express.static(__dirname + '/node_modules'));
+        app.post('/login', function (req, res, next) {
+          passport.authenticate('local', function (err, user, info) {
+            if (err) {
+              return next(err);
+            }
+
+            var status, type, send;
+            if (!user) {
+              status = 401;
+              type   = 'text/plain';
+              send   = 'usuario o password incorrecto';
+            } else {
+              status = 200;
+              type   = 'text/plain';
+              send   = 'usuario logado correctamente';
+            }
+
+            res.status(status);
+            res.type(type);
+            res.send(send);
+
+          })(req, res, next);
+        });
+
+
+        /*****************************FIN ROUTING *****************************/
 
         app.listen(3000, function () {
           console.log('Servidor escuchando en http://localhost:3000');
