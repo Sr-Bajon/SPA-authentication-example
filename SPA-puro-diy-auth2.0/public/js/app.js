0a6f626e6248942f977afb92d12e6b8446004dc2
Ismael-Rodriguez <ismael.dai@gmail.com>
2015-12-17 11:50:06 +0100

    ejemplo funcionando

--- SPA-puro-diy-auth2.0/public/js/app.js
+++ SPA-puro-diy-auth2.0/public/js/app.js
@@ -1,11 +1,28 @@
 angular.module('myApp', ['ui.router'])
+
   .config(function ($provide) {
     'use strict';
+
+    // decora $state para esté disponible en el resolve el parametro next y toParams para conocer
+    // hacia que ruta se dirige la transición.
+    // http://stackoverflow.com/questions/22985988/angular-ui-router-get-state-info-of-tostate-in-resolve
     $provide.decorator('$state', function ($delegate, $rootScope) {
       $rootScope.$on('$stateChangeStart', function (event, state, params) {
         $delegate.next     = state;
         $delegate.toParams = params;
       });
+
+      // decora $state para que la opcion reload:true esté por defecto y recargue el resolve en cada
+      // peticion
+      // http://stackoverflow.com/questions/22730868/ui-routers-resolve-functions-are-only-called-once#
+/*      var originalTransitionTo = $delegate.transitionTo;
+      $delegate.transitionTo   = function (to, toParams, options) {
+        return originalTransitionTo(to, toParams, angular.extend({
+          reload: true,
+          notify: true
+        }, options));
+      };*/
+
       return $delegate;
     });
   })
@@ -24,17 +41,24 @@ angular.module('myApp', ['ui.router'])
         .state('principal', {
           url        : '/principal',
           templateUrl: 'views/principal.html',
-          resolve    : {authenticate: 'AuthService'}
+          resolve    : {authenticate: authFunction}
         })
         .state('admin', {
           url        : '/admin',
           templateUrl: 'views/admin.html',
-          resolve    : {authenticate: 'AuthService'}
+          resolve    : {authenticate: authFunction}
         })
         .state('indice', {
           url        : '/indice',
           templateUrl: 'views/indice.html'
         });
+
+      function authFunction($http, $state) {
+        return $http.post('/authenticate', {
+          url: $state.next.url
+        });
+      }
+
     }])
 
   .run(['$rootScope', '$state', function ($rootScope, $state) {
@@ -44,16 +68,9 @@ angular.module('myApp', ['ui.router'])
       function (event, toState, toParams, fromState, fromParams, error) {
         event.preventDefault();
         //$state.transitionTo('login');
+        //$state.go('login', {}, {reload: true, notify: true});
         $state.go('login');
       });
-  }])
-
-  .factory('AuthService', ['$http', '$state', function ($http, $state) {
-    'use strict';
-    console.log('hasta aqui');
-    return $http.post('/authenticate', {
-      url: $state.next.url
-    });
   }])
 
   .controller('FormController', ['$http', function ($http) {
@@ -68,13 +85,11 @@ angular.module('myApp', ['ui.router'])
         username: formCtrl.formulario.nombre,
         password: formCtrl.formulario.password
       }).then(function (success) {
-        1 + 1;
+        formCtrl.loggued = true;
       }, function (err) {
-        1 + 1;
+        formCtrl.loggued = false;
       });
     };
-
-
   }])
 
   .controller('IndiceController', ['$http', function ($http) {
@@ -85,9 +100,9 @@ angular.module('myApp', ['ui.router'])
     indiceCtrl.logout = function () {
       $http.post('/logout')
         .then(function (success) {
-          1 + 1;
+          indiceCtrl.loggedOut = true;
         }, function (err) {
-          1 + 1;
+          indiceCtrl.loggedOut = false;
         });
     };
 
