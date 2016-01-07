a34283887d1072236676c17acfe66132c077d6b9
IsmaelR <ismael.dai@mail.com>
2016-01-07 22:59:54 +0100

    promisificacion

--- SPA-puro-diy-auth-DB-session-promises/ejemplos.js
+++ SPA-puro-diy-auth-DB-session-promises/ejemplos.js
@@ -22,9 +22,37 @@
     return new Promise.resolve(miPromesa().then(miOtraPromesa));
   }
 
-  concatena().then(function(data){
+  concatena().then(function (data) {
     console.log(data);
   });
+
+
+  function promesaConcatenable() {
+    return new Promise(function (resolve) {
+      var x = 2 + 2;
+      resolve(x);
+    });
+  }
+
+  function promesaContenable2(data) {
+    return new Promise(function (resolve) {
+      console.log(data + 4);
+      resolve(data + 4);
+    });
+  }
+
+  promesaConcatenable().then(suma);
+  promesaConcatenable().then(suma(33));
+  promesaConcatenable().then(promiseAll);
+
+
+  function suma(dato){
+    console.log(dato);
+  }
+
+  function promiseAll(data){
+    return Promise.all([promesaContenable2(data), promesaContenable2(data), promesaContenable2(data)]);
+  }
 
 })();
 
